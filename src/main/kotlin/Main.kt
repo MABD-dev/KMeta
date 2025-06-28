aa0db67e3b7c9ed0711f5cf1a2a62e400a830e92
MABD <mabd.universe@gmail.com>
2025-06-28 10:23:37 +0300

    Created main file for loggable processor

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,51 +1,7 @@
 package org.mabd
 
 import org.mabd.mimic_data_class.Copy
-import org.mabd.loggable.Loggable
-import org.mabd.loggable.NoLog
 
-
-/**
- * Testing docs on class
- */
-@Loggable(
-    tag = "MyLogTag"
-)
-interface ApiService<T> {
-
-    var isAuth: Boolean?
-
-    @Deprecated(
-        message = "just for testing",
-        replaceWith = ReplaceWith("test2(1)")
-    )
-    fun test()
-
-    @NoLog
-    fun test2(a: Int)
-
-    /**
-     * Testing docs on function with params and return
-     * @param b something
-     * @return some number
-     */
-    fun test3(b: Int): Int
-
-    fun <T: Number, R> test4()
-
-    fun test5(a: Int, vararg f: Float)
-}
-
-class RealApiService: ApiService<String> {
-    override var isAuth: Boolean? = false
-
-    override fun test() {}
-    override fun test2(a: Int) {}
-    override fun test3(b: Int): Int { return 1 }
-    override fun <T: Number, R> test4() {}
-    override fun test5(a: Int, vararg f: Float) {}
-
-}
 
 @Copy
 class User(
@@ -54,22 +10,11 @@ class User(
 )
 
 fun main() {
-    val apiService = ApiServiceLoggerImpl(RealApiService())
-
-    println(apiService.isAuth)
-    apiService.isAuth = true
-
-    apiService.test()
-    apiService.test2(1)
-    apiService.test3(3)
-    apiService.test4<Int, Int>()
-    apiService.test5(1, .1f, .2f)
-
-    val user = User(1, "someone")
-    val user2 = user.copy(age = 10)
-
-    println(user.toNiceString())
-    println(user2.toNiceString())
+//    val user = User(1, "someone")
+//    val user2 = user.copy(age = 10)
+//
+//    println(user.toNiceString())
+//    println(user2.toNiceString())
 
 }
 
---
+++ src/main/kotlin/examples/loggable/RealApiService.kt
@@ -0,0 +1,26 @@
+package org.mabd.examples.loggable
+
+//
+/**
+ * mimics real implementation of [ApiService]
+ */
+internal class RealApiService: ApiService<String> {
+    override var isAuth: Boolean? = false
+
+    override fun testAnnotationsArePreserved() {
+        // Real implementation goes here
+    }
+    override fun testNoLogGenerated(a: Int) {
+        // Real implementation goes here
+    }
+    override fun testDocsArePreserved(b: Int): Int {
+        // Real implementation goes here
+        return 1
+    }
+    override fun <T: Number, R> testGenericsArePossible() {
+        // Real implementation goes here
+    }
+    override fun testVararg(a: Int, vararg f: Float) {
+        // Real implementation goes here
+    }
+}
